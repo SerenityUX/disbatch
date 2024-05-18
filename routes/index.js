4cf24ca9379a6d2d05cbea4fff504c3f5e2e95ce
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-05-18 18:36:46 -0400

    screams by ID

--- routes/index.js
+++ routes/index.js
@@ -11,24 +11,34 @@ let screams = [];
 
 router.get('/getNewScreams', (req, res) => {
     
-    const hasBeenPlayedNot = screams.filter((scream) => !scream.hasBeenPlayed)
+    const receiver = req.query.receiver
+    const hasBeenPlayedNotAndIsMine = screams.filter((scream) => !scream.hasBeenPlayed && scream.receiver == receiver)
 
     screams.forEach((scream) => {
+      if(scream.receiver == receiver) {
         scream.hasBeenPlayed = true;
+      }
     });
     
-    res.send({ screams: hasBeenPlayedNot });
+    res.send({ screams: hasBeenPlayedNotAndIsMine });
     
 });
 
 router.post('/createScream', (req, res) => {
+
+    if(req.query.receiver == null) {
+      console.log(req)
+      res.send({message: "ERROR WE NEED RECEIVER"});
+    } else {
     const randomID = Math.random() * 100000;
     screams.push({
         name: "zombie",
         hasBeenPlayed: false,
-        id: randomID
+        id: randomID,
+        receiver: req.query.receiver
     });
     res.send(screams);
+  }
 });
 
 module.exports = router;
